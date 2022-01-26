86d0eeea6e58ff8522485bf5cb07244e2184fbc4
Coding-man-Chen <729178954@qq.com>
2022-01-26 20:25:58 +0100

    simplify pages of comment, explore and project

--- v5/src/pages/comment.vue
+++ v5/src/pages/comment.vue
@@ -1,22 +1,25 @@
 <template>
   <f7-page name="comment">
     <!-- Navbar area -->
-    <f7-navbar title="Comment" back-link="Back" style="font-size: 20px" @click='deleteBadge()'></f7-navbar>
+    <f7-navbar
+      title="Comment"
+      back-link="Back"
+      style="font-size: 20px"
+      @click="deleteBadge()"
+    ></f7-navbar>
     <f7-block>
       <!-- Comment content -->
       <f7-list media-list style="height: 150px">
-        <f7-list-item 
-          v-for="item in list" v-bind:key="item" 
-          v-bind:title="item.name" 
-          v-bind:after="item.date" 
+        <f7-list-item
+          v-for="item in list"
+          v-bind:key="item"
+          v-bind:title="item.name"
+          v-bind:after="item.date"
           v-bind:subtitle="item.text"
         >
           <template #media>
-            <img
-              v-bind:src="item.avatar"
-              class="avatar"
-            />
-            <i class="badgeToimg" v-show='item.badge'></i>
+            <img v-bind:src="item.avatar" class="avatar" />
+            <i class="badgeToimg" v-show="item.badge"></i>
           </template>
         </f7-list-item>
         <br />
@@ -65,124 +68,132 @@
   </f7-page>
 </template>
 <style scoped>
-.badgeToimg{
+/* Style in this page */
+.badgeToimg {
   display: flex;
   height: 20px;
-  width:20px;
+  width: 20px;
   background-color: #4cd964;
   border-radius: 50%;
   position: absolute;
   left: 70px;
   top: 10px;
 }
-.avatar{
-  border-radius: 50%; 
+.avatar {
+  border-radius: 50%;
   width: 20vmin;
-  height: 20vmin; 
-  object-fit: cover; 
+  height: 20vmin;
+  object-fit: cover;
   object-position: center;
 }
-.media-list{
+.media-list {
   --f7-list-item-title-font-size: 23px;
   --f7-list-item-after-font-size: 20px;
   --f7-list-item-subtitle-font-size: 20px;
 }
 </style>
 <script>
-import { f7Button,f7 } from 'framework7-vue';
-import myBus from '../js/myBus.js';
-const self =this;
+import { f7Button, f7 } from "framework7-vue";
+import myBus from "../js/myBus.js";
+const self = this;
 export default {
   props: {
-      f7route: Object,
-      f7router: Object,
-    },
+    f7route: Object,
+    f7router: Object,
+  },
   components: {
     f7Button,
   },
-data(){
+  data() {
     return {
       messageText: "",
       // initial comment data
-      list:[
-              { name:"Alice Wilson",
-                avatar: "https://images.pexels.com/photos/1130626/pexels-photo-1130626.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-                date: "02/02/2022",
-                text: "Thanks,I will try it on my father's car.",
-                id:5,
-                badge: false,
-              },
-              { name:"Eric Bach",
-                avatar: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-                date: "04/12/2021",
-                text: "Thanks for sharing!",
-                id:2,
-                badge: false,
-              },
-              { name:"Christina Ruck",
-                avatar: "https://images.pexels.com/photos/38554/girl-people-landscape-sun-38554.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-                date: "30/11/2021",
-                text: "The tips are all useless for me.",
-                id:3,
-                badge: false,
-              },
-              { name:"Jeson Smith",
-                avatar: "https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-                date: "25/11/2021",
-                text: "Cool idea! I will try it next time :)",
-                id:4,
-                badge: false,
-              },
-              { name:"Hans Schmitt",
-                avatar: "https://images.pexels.com/photos/5486199/pexels-photo-5486199.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-                date: "15/11/2021",
-                text: "At least I need one my own car",
-                id:6,
-                badge: false,
-              },
-            ],
-      newComment:{
-                name:"Jessi Briem",
-                avatar: "https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-                date: "",
-                text: "",
-                id:7,
-      }
-    }
-},
-  computed: {
-    activeList() {
-      return this.list.filter((item) => item.id < 50)
-    }
+      list: [
+        {
+          name: "Alice Wilson",
+          avatar:
+            "https://images.pexels.com/photos/1130626/pexels-photo-1130626.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+          date: "02/02/2022",
+          text: "Thanks,I will try it on my father's car.",
+          id: 5,
+          badge: false,
+        },
+        {
+          name: "Eric Bach",
+          avatar:
+            "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+          date: "04/12/2021",
+          text: "Thanks for sharing!",
+          id: 2,
+          badge: false,
+        },
+        {
+          name: "Christina Ruck",
+          avatar:
+            "https://images.pexels.com/photos/38554/girl-people-landscape-sun-38554.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+          date: "30/11/2021",
+          text: "The tips are all useless for me.",
+          id: 3,
+          badge: false,
+        },
+        {
+          name: "Jeson Smith",
+          avatar:
+            "https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+          date: "25/11/2021",
+          text: "Cool idea! I will try it next time :)",
+          id: 4,
+          badge: false,
+        },
+        {
+          name: "Hans Schmitt",
+          avatar:
+            "https://images.pexels.com/photos/5486199/pexels-photo-5486199.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+          date: "15/11/2021",
+          text: "At least I need one my own car",
+          id: 6,
+          badge: false,
+        },
+      ],
+      // data of new comment
+      newComment: {
+        name: "Jessi Briem",
+        avatar:
+          "https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        date: "",
+        text: "",
+        id: 7,
+      },
+    };
   },
-  beforeCreate(){
-    myBus.on("ToComment",data => {
+  beforeCreate() {
+    // get latest Comment from page project
+    myBus.on("ToComment", (data) => {
       myBus.newComment = data;
-    })
+    });
   },
-  mounted() { 
-    if(typeof myBus.newComment !== "undefined")
-     this.list.unshift(myBus.newComment);
+  mounted() {
+    // push the new Comment 
+    if (typeof myBus.newComment !== "undefined")
+      this.list.unshift(myBus.newComment);
   },
   methods: {
-    addComment(){
-              var nowDate = new Date();
-              let date = {
-                  year: nowDate.getFullYear(),
-                  month: nowDate.getMonth() + 1,
-                   date: nowDate.getDate(),
-               }
-              let systemDate = date.date + '/' + 0 + date.month + '/'  + date.year;
-      this.newComment.date= systemDate;
-      this.newComment.text= this.messageText;
-      this.list.unshift(this.newComment)
-      myBus.emit("ToProject",this.newComment)
+    // Add new comment from the messagebox
+    addComment() {
+      var nowDate = new Date();
+      let date = {
+        year: nowDate.getFullYear(),
+        month: nowDate.getMonth() + 1,
+        date: nowDate.getDate(),
+      };
+      let systemDate = date.date + "/" + 0 + date.month + "/" + date.year;
+      this.newComment.date = systemDate;
+      this.newComment.text = this.messageText;
+      this.list.unshift(this.newComment);
+      myBus.emit("ToProject", this.newComment);
       this.messageText = "";
     },
-    test(){
-      console.log(this.newComment)
-    }
   },
-}
+};
 </script>
 
--- v5/src/pages/project.vue
+++ v5/src/pages/project.vue
@@ -1,10 +1,14 @@
 <template>
   <f7-page name="project">
     <!-- Navbar area -->
-    <f7-navbar title="Project" back-link="Back" style="font-size: 20px"></f7-navbar>
-    <f7-block style="text-align: center" >
+    <f7-navbar
+      title="Project"
+      back-link="Back"
+      style="font-size: 20px"
+    ></f7-navbar>
+    <f7-block style="text-align: center">
       <!-- Swiper for pictures -->
-      <f7-swiper pagination >
+      <f7-swiper pagination>
         <f7-swiper-slide>
           <img
             v-bind:src="getItem.img_first"
@@ -33,28 +37,42 @@
     </f7-block>
     <hr />
     <!-- Article for project -->
-    <f7-block >
-      <h2 style="text-align: center; font-size: 30px" >{{getItem.title}}</h2>
-      <p class="Content" style="font-size: 20px">{{getItem.text}}</p>
+    <f7-block>
+      <h2 style="text-align: center; font-size: 30px">{{ getItem.title }}</h2>
+      <p class="Content" style="font-size: 20px">{{ getItem.text }}</p>
       <!-- Description of Author -->
       <h3 style="position: relative; top: 10px; font-size: 25px">Author</h3>
       <div style="position: absolute; bottom: 200px; right: 20px">
-        <f7-icon v-bind:f7="getItem.like_type" size="25px" class="black_heart" @click="ChangeLike()"></f7-icon>
-        <span style="position: relative; top: 5px; font-size: 20px"> {{getItem.likes}} </span>
+        <f7-icon
+          v-bind:f7="getItem.like_type"
+          size="25px"
+          class="black_heart"
+          @click="ChangeLike()"
+        ></f7-icon>
+        <span style="position: relative; top: 5px; font-size: 20px">
+          {{ getItem.likes }}
+        </span>
       </div>
       <a class="link" href="/profile-helper/">
-      <f7-list inset media-list style="height: 100px; position: relative; width: 325px">
-        <f7-list-item v-bind:title="getItem.writer" text="Expert of car repair">
-          <br>       
-          <template #media>
-            <img
-              src="https://images.pexels.com/photos/1181686/pexels-photo-1181686.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-              width="40"
-              class="avatar"
-            />
-          </template>
-        </f7-list-item>
-      </f7-list>
+        <f7-list
+          inset
+          media-list
+          style="height: 100px; position: relative; width: 325px"
+        >
+          <f7-list-item
+            v-bind:title="getItem.writer"
+            text="Expert of car repair"
+          >
+            <br />
+            <template #media>
+              <img
+                src="https://images.pexels.com/photos/1181686/pexels-photo-1181686.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                width="40"
+                class="avatar"
+              />
+            </template>
+          </f7-list-item>
+        </f7-list>
       </a>
     </f7-block>
     <hr />
@@ -63,7 +81,8 @@
       <h3 style="padding: 0px; font-size: 25px">Last comment</h3>
       <f7-list
         media-list
-        style="height: 100px; position: relative; bottom: 20px" inset
+        style="height: 100px; position: relative; bottom: 20px"
+        inset
       >
         <f7-list-item
           v-bind:title="latestComment.name"
@@ -71,18 +90,11 @@
           v-bind:after="latestComment.date"
         >
           <template #media>
-            <img
-              v-bind:src="latestComment.avatar"
-              width="28"
-              class="avatar"
-            />
+            <img v-bind:src="latestComment.avatar" width="28" class="avatar" />
           </template>
         </f7-list-item>
       </f7-list>
-      <f7-button
-        fill
-        href="/comment/"
-        style="position: relative; bottom: 40px"
+      <f7-button fill href="/comment/" style="position: relative; bottom: 40px"
         >Show all comments</f7-button
       >
       <br />
@@ -103,7 +115,7 @@
               icon-ios="f7:arrow_up_circle_fill"
               icon-aurora="f7:arrow_up_circle_fill"
               icon-md="material:send"
-               @click="addComment"
+              @click="addComment"
             />
           </template>
           <f7-messagebar-attachments>
@@ -130,6 +142,7 @@
 </template>
 
 <style scoped>
+/* Style in this page */
 .user {
   color: grey;
   font-size: 5px;
@@ -143,98 +156,104 @@
   float: right;
   margin-top: 5px;
 }
-.avatar{
-  border-radius: 50%; 
+.avatar {
+  border-radius: 50%;
   width: 15vmin;
-   height: 15vmin; 
-   object-fit: cover; 
-   object-position: center;
+  height: 15vmin;
+  object-fit: cover;
+  object-position: center;
 }
-.media-list{
+.media-list {
   --f7-list-item-title-font-size: 25px;
   --f7-list-item-text-font-size: 20px;
 }
-.button{
-  --f7-button-bg-color: #468DCE;
+.button {
+  --f7-button-bg-color: #468dce;
   --f7-button-font-size: 15px;
 }
-
 </style>
 
 <script>
-import { f7Button,f7 } from 'framework7-vue';
-import myBus from '../js/myBus.js';
-import routes from "../js/routes.js";
+import { f7Button, f7 } from "framework7-vue";
+import myBus from "../js/myBus.js";
 export default {
   props: {
-      f7route: Object,
-      f7router: Object,
-    },
+    f7route: Object,
+    f7router: Object,
+  },
   components: {
     f7Button,
-    f7
+    f7,
   },
-  data(){
-      return {
-        messageText: "",
-        getItem:{ title:"1",
-                  img_first: "https://imgs.wantubizhi.com/upload/i_0/T1huMFdJZ3Y2V3VzVloxUEtCaExYZz09/3009539753x1910106365_26_0.jpg",
-                  img_second: "https://media.istockphoto.com/photos/mechanic-using-a-ratchet-wrench-picture-id1165311626?k=20&m=1165311626&s=612x612&w=0&h=2bA0oO8I6mNN7QlZHeCRTs9tRbMae9JNSsvdq-zD1Wg=",
-                  img_third:"https://media.istockphoto.com/photos/mechanic-using-a-ratchet-wrench-picture-id1165311626?k=20&m=1165311626&s=612x612&w=0&h=2bA0oO8I6mNN7QlZHeCRTs9tRbMae9JNSsvdq-zD1Wg=",
-                  text:"2",
-                  writer: '3',
-                  likes: '4',
-                  like_type: "",
-                  id:2,
-                },
-        title: '',
-        latestComment:{
-                name:"Alice Wilson",
-                avatar: "https://images.pexels.com/photos/1130626/pexels-photo-1130626.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-                date: "02/02/2022",
-                text: "Thanks,I will try it on my father's car.",
-                id:5,
-        }
-      }
+  data() {
+    return {
+      messageText: "",
+      // Default the project page
+      getItem: {
+        title: "1",
+        img_first:
+          "https://imgs.wantubizhi.com/upload/i_0/T1huMFdJZ3Y2V3VzVloxUEtCaExYZz09/3009539753x1910106365_26_0.jpg",
+        img_second:
+          "https://media.istockphoto.com/photos/mechanic-using-a-ratchet-wrench-picture-id1165311626?k=20&m=1165311626&s=612x612&w=0&h=2bA0oO8I6mNN7QlZHeCRTs9tRbMae9JNSsvdq-zD1Wg=",
+        img_third:
+          "https://media.istockphoto.com/photos/mechanic-using-a-ratchet-wrench-picture-id1165311626?k=20&m=1165311626&s=612x612&w=0&h=2bA0oO8I6mNN7QlZHeCRTs9tRbMae9JNSsvdq-zD1Wg=",
+        text: "2",
+        writer: "3",
+        likes: "4",
+        like_type: "",
+        id: 2,
+      },
+      // Content of lastest comment
+      latestComment: {
+        name: "Jessi Briem",
+        avatar:
+          "https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        date: "02/02/2022",
+        text: "Thanks,I will try it on my father's car.",
+        id: 5,
+      },
+    };
   },
   mounted() {
-    myBus.on("Id",data => {
-      this.getItem = data
-      console.log(this.getItem)
-           })
-    myBus.on("ToProject",data => {
-      this.latestComment = data
-           })    
+    // Get project content from page explore
+    myBus.on("Id", (data) => {
+      this.getItem = data;
+    });
+    // Get latest comment from page comment
+    myBus.on("ToProject", (data) => {
+      this.latestComment = data;
+    });
   },
 
-  methods:{
-      addComment(){
-              var nowDate = new Date();
-              let date = {
-                  year: nowDate.getFullYear(),
-                  month: nowDate.getMonth() + 1,
-                   date: nowDate.getDate(),
-               }
-              console.log(date);
-              let systemDate = date.date + '/' + 0 + date.month + '/'  + date.year;
-      this.latestComment.date= systemDate;
-      this.latestComment.text= this.messageText;
-      myBus.emit("ToComment",this.latestComment)
+  methods: {
+    // Add new comment from the messagebox
+    addComment() {
+      var nowDate = new Date();
+      let date = {
+        year: nowDate.getFullYear(),
+        month: nowDate.getMonth() + 1,
+        date: nowDate.getDate(),
+      };
+      console.log(date);
+      let systemDate = date.date + "/" + 0 + date.month + "/" + date.year;
+      this.latestComment.date = systemDate;
+      this.latestComment.text = this.messageText;
+      myBus.emit("ToComment", this.latestComment);
       this.messageText = "";
     },
-    ChangeLike(){
-      if(this.getItem.like_type=="suit_heart")
-      {
-        this.getItem.like_type="suit_heart_fill";
+    // Change the status of liked dynamically
+    ChangeLike() {
+      if (this.getItem.like_type == "suit_heart") {
+        this.getItem.like_type = "suit_heart_fill";
         this.getItem.likes++;
-      }
-      else{
-        this.getItem.like_type="suit_heart";
+      } else {
+        this.getItem.like_type = "suit_heart";
         this.getItem.likes--;
       }
-      myBus.emit("ChangeLike",this.getItem)
-    }
+      // Transport change of status to page gallery and explore
+      myBus.emit("ChangeLike", this.getItem);
+    },
   },
-}
+};
 </script>
 
